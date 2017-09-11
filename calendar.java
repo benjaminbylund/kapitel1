2a0c03fd8abe924807ba69ee812ff82d90360721
Benjamin Bylund <bybe16@nti.umea>
2017-09-11 10:51:33 +0200

    uppgift 1.1

---
+++ calendar.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.util.*;
+public class calendar{
+ public static void main(String[] args) {
+ 	JOptionPane.showMessageDialog(null, "Datum och Tid: \n" + Calendar.getInstance().getTime().toString());
+ 	
+ 
+ 	}
+}
