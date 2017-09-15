3d64a8effb61980ab52259b92953c399a03fe4e7
Benjamin Bylund <bybe16@nti.umea>
2017-09-15 21:59:40 +0200

    gjort uppgift 1.2

--- ShowMessageDialog.java
+++ ShowMessageDialog.java
@@ -2,10 +2,16 @@ import javax.swing.*;
 import java.awt.Toolkit;
 public class ShowMessageDialog{
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"Benjamin"
-			+ "\nDu kör Windows 10");
-		JOptionPane.showMessageDialog(null,"070123456");
-		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null,"Benjamin" + "\nOlofpalmegata" + "\n0701234567");
+		//JOptionPane.showMessageDialog(null,"Benjamin"
+		//	+ "\nDu kör Windows 10");
+		//JOptionPane.showMessageDialog(null,"070123456");
+		//Toolkit.getDefaultToolkit().beep();
+		//JOptionPane.showMessageDialog(null,"Benjamin" + "\nOlofpalmegata" + "\n0701234567"); 
+		 JOptionPane.showMessageDialog(null, "Information", "Information",
+         	JOptionPane.QUESTION_MESSAGE);
+         JOptionPane.showMessageDialog(null, "Varning", "Varning",
+         	JOptionPane.WARNING_MESSAGE);
+         JOptionPane.showMessageDialog(null, "Fel", "Fel",
+         	JOptionPane.ERROR_MESSAGE);
 	}
 }
